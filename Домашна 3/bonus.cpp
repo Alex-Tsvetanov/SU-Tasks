e2a3f3d0ee36ed7abe6f9e1c43768e80e9ed07c2
Alex Tsvetanov <a.tsvetanov@headstarter.eu>
2020-01-06 06:20:04 +0200

    set up bonus.cpp

---
+++ Домашна 3/bonus.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+bool bmp_to_grayscale(const char * name)
+{
+    const char* outputFile = "output.bmp";
+}
+
+int main ()
+{
+    bmp_to_greyscale("test_image.bmp");
+}
